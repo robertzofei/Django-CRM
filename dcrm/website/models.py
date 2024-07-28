1ef3d1f6099aa1656a961987bd98bdf7711aef59
robertzofei <rzofei@gmail.com>
2024-07-28 16:19:54 +0300

    Final Project

--- dcrm/website/models.py
+++ dcrm/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	first_name = models.CharField(max_length = 64)
+	last_name = models.CharField(max_length = 64)
+	email = models.CharField(max_length = 64)
+	phone = models.CharField(max_length = 16)
+	address = models.CharField(max_length = 128)
+	city = models.CharField(max_length = 64)
+	county = models.CharField(max_length = 64)
+	zipcode = models.CharField(max_length = 16)
+
+	def __str__(self):
+		return (f"{self.first_name} {self.last_name}")
